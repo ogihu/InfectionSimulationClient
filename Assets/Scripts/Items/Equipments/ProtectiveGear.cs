385a354a41a784c0e984a0f77c49c775977e1039
GGolDDuKi <dduki0729@gmail.com>
2024-07-24 11:15:39 +0900

    commit

---
+++ Assets/Scripts/Items/Equipments/ProtectiveGear.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProtectiveGear : Equipment
+{
+    public override void Equip(BaseController character)
+    {
+        base.Equip(character);
+
+        Transform parent = Util.FindChildByName(character.gameObject, "basic_rig Spine").transform;
+        if (parent == null)
+        {
+            Debug.Log("Can't find transform");
+            return;
+        }
+
+        gameObject.transform.SetParent(parent.transform, false);
+    }
+}

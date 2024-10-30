b8ba156710edd88d76f073f3262017a28189f483
Kim-Minuk <dduki0729@gmail.com>
2024-10-30 16:35:24 +0900

    비밀번호 입력 공개 상태 구현

--- Assets/Scripts/UI/LoginUI/PwInputField.cs
+++ Assets/Scripts/UI/LoginUI/PwInputField.cs
@@ -10,24 +10,5 @@ public class PwInputField : MonoBehaviour
     private void Awake()
     {
         _inputField = GetComponent<TMP_InputField>();
-        _inputField.onValueChanged.AddListener(OnInputValueChanged);
-    }
-
-    void OnInputValueChanged(string input)
-    {
-        // 입력된 문자열을 한 글자씩 영어로 변환
-        char[] convertedChars = new char[input.Length];
-        for (int i = 0; i < input.Length; i++)
-        {
-            convertedChars[i] = Define.ChangeKtoE(input[i]);
-        }
-
-        // 변환된 문자열을 InputField에 반영
-        string convertedText = new string(convertedChars);
-        if (input != convertedText)
-        {
-            _inputField.text = convertedText;
-            _inputField.caretPosition = convertedText.Length;
-        }
     }
 }

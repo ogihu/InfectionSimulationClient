131f954af15476a6b73f9a3f803b68ca6c392e90
jin45142426 <85512890+jin45142426@users.noreply.github.com>
2024-11-06 16:57:27 +0900

    commit

--- Assets/Swab.cs
+++ Assets/Swab.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Swab : UsingItem
 {
+    bool check = false;
+    public Texture2D cursorTexture;  // 사용할 커서 이미지
+    public Vector2 hotSpot = Vector2.zero; // 커서의 포인터 위치 설정 (0,0은 좌상단)
     public override bool Use(BaseController character)
     {
+        cursorTexture = 
         StartCoroutine(Use_Swab());
+        StartCoroutine(Use_Checking());
         return base.Use(character);
     }
 
     IEnumerator Use_Swab()
     {
-
+        Cursor.SetCursor(cursorTexture, hotSpot, CursorMode.Auto);
         yield return null;
+    }
+    IEnumerator Use_Checking()
+    {
+        while(!check)
+            yield return null;
     }
 }

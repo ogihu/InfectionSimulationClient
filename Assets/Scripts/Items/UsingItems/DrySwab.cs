4bf679496c9bb3950a190d38925c428e4a75fa8d
jin45142426 <85512890+jin45142426@users.noreply.github.com>
2024-11-08 17:33:21 +0900

    Commit

--- Assets/Scripts/Items/UsingItems/DrySwab.cs
+++ Assets/Scripts/Items/UsingItems/DrySwab.cs
@@ -1,9 +1,11 @@
+using Google.Protobuf.Protocol;
 using Ricimi;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.TextCore.Text;
 using UnityEngine.UI;
 using static Google.Cloud.Speech.V1.LanguageCodes;
 
@@ -12,6 +14,7 @@ public class DrySwab : UsingItem
     Texture2D cursorTexture;  // 사용할 커서 이미지
     GameObject popup;
     GameObject lesion;
+    GameObject obj;
     public bool check = false;
 
     public override bool Use(BaseController character)
@@ -28,6 +31,7 @@ public class DrySwab : UsingItem
         popup = Managers.UI.CreateUI("PopupNotice");
 
         //@@@@@@@@@@이 부분 환자 클릭하면 면봉 사용 UI 표시되게 해야함. 혈액채취 부분도 마찬가지@@@@@@@@@@
+        StartCoroutine(StartChecking());
         //카운트다운 시작
         for (i = 3; i > 0; i--)
         {
@@ -43,6 +47,9 @@ public class DrySwab : UsingItem
         lesion = Managers.UI.CreateUI("LesionUI");
         lesion.GetComponent<LesionUI>().Swab = this;
 
+        //면봉 상태(애니메이션)
+        //Mycharacter.State = CreatureState.Syringe;
+
         Vector2 hotSpot = new Vector2(0, cursorTexture.height);
         Cursor.SetCursor(cursorTexture, hotSpot, CursorMode.ForceSoftware);
     }
@@ -55,5 +62,51 @@ public class DrySwab : UsingItem
         Managers.Scenario.MyAction = "Use";
         Managers.Scenario.Item = "DrySwab";
         Managers.Scenario.Targets.Add("환자");
+    }
+    IEnumerator PassCheck()
+    {
+        // 면봉 캐릭터 손에 부여
+        while (!check)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                // 바라보는 대상 확인
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, transform.forward, out hit))
+                {
+                    // 바라보는 대상의 이름이 "환자"일 경우
+                    if (hit.transform.gameObject.name == "환자")
+                    {
+                        obj = hit.transform.gameObject;
+                    }
+                }
+                else
+                {
+                    if (obj != null)
+                    {
+                        obj = null;
+                    }
+                }
+                // 시나리오 액션이 "Use"일 때
+                if (Managers.Scenario.CurrentScenarioInfo.Action == "Use")
+                {
+                    // 올바른 오브젝트 클릭 체크
+                    if (obj != null && obj.transform.gameObject.name == "환자")
+                    {
+                        // 시나리오 정보가 유효할 때
+                        if (Managers.Scenario.CurrentScenarioInfo != null)
+                        {
+                            check = true; 
+                        }
+                    }
+                    else if (!Managers.Item.IsInventoryOpen)
+                    {
+                        Managers.UI.CreateSystemPopup("WarningPopup", "올바른 곳에 사용하세요.", UIManager.NoticeType.Warning);
+                    }
+                }
+            }
+            // 다음 프레임까지 대기
+            yield return null;
+        }
     }
 }
--- Assets/Scripts/Items/UsingItems/Syringe.cs
+++ Assets/Scripts/Items/UsingItems/Syringe.cs
@@ -24,7 +24,6 @@ public class Syringe : UsingItem
     public override void Init()
     {
         base.Init();
-        _layerPoint = 1 << LayerMask.NameToLayer("Point");
     }
 
     public override bool Use(BaseController character)
@@ -42,58 +41,11 @@ public class Syringe : UsingItem
             return true;
 
         check = false;
+        go = gameObject;
 
         if (Mycharacter == null)
             Mycharacter = character;
-        if (go == null)
-        {
-            go = gameObject;
-            Point = Managers.Scenario.NPCs["환자"].transform.GetChild(1).gameObject;
-            Point.SetActive(true);
-            Point.transform.localScale = new Vector3(0.0764116f, 0.009552589f, 0.07533843f);
-            Transform point = Point.transform.GetChild(0);
-            points.Add(point.gameObject);
-            point.SetParent(Util.FindChildByName(Managers.Scenario.NPCs["환자"].gameObject, "basic_rig R Forearm").transform, false);
-            point.transform.localPosition = new Vector3(-0.0261f, -0.0131f, -0.0409f);
-            point.transform.localRotation = Quaternion.Euler(83.043f, -0.499f, -0.359f);
-            point.transform.localScale = new Vector3(0.0764116f, 0.01577159f, 0.07533843f);
-
-            point = Point.transform.GetChild(0); points.Add(point.gameObject);
-            Point.transform.GetChild(0).SetParent(Util.FindChildByName(Managers.Scenario.NPCs["환자"].gameObject, "basic_rig L Forearm").transform, false);
-            point.transform.localPosition = new Vector3(-0.0232f, -0.0232f, 0.0377f);
-            point.transform.localRotation = Quaternion.Euler(109.183f, 21.666f, 21.255f);
-            point.transform.localScale = new Vector3(0.0764116f, 0.01577159f, 0.07533843f);
-
-            point = Point.transform.GetChild(0); points.Add(point.gameObject);
-            Point.transform.GetChild(0).SetParent(Util.FindChildByName(Managers.Scenario.NPCs["환자"].gameObject, "basic_rig L UpperArm").transform, false);
-            point.transform.localPosition = new Vector3(0.006689013f, -0.02759328f, 0.01746411f);
-            point.transform.localRotation = Quaternion.Euler(90f, 0, 0);
-            point.transform.localScale = new Vector3(0.0764116f, 0.01577159f, 0.07533843f);
-
-            point = Point.transform.GetChild(0); points.Add(point.gameObject);
-            Point.transform.GetChild(0).SetParent(Util.FindChildByName(Managers.Scenario.NPCs["환자"].gameObject, "basic_rig UpperArm").transform, false);
-            point.transform.localPosition = new Vector3(-0.0036f, -0.0311f, -0.0329f);
-            point.transform.localRotation = Quaternion.Euler(64.809f, -5.752f, -1.137f);
-            point.transform.localScale = new Vector3(0.0764116f, 0.01577159f, 0.07533843f);
-
-            point = Point.transform.GetChild(0); points.Add(point.gameObject);
-            Point.transform.GetChild(0).SetParent(Util.FindChildByName(Managers.Scenario.NPCs["환자"].gameObject, "basic_rig R Calf").transform, false);
-            point.transform.localPosition = new Vector3(0.0195f, 0.0195f, 0.0005f);
-            point.transform.localRotation = Quaternion.Euler(202.019f, 180.416f, 179.297f);
-            point.transform.localScale = new Vector3(0.0764116f, 0.01577159f, 0.07533843f);
-
-            point = Point.transform.GetChild(0); points.Add(point.gameObject);
-            Point.transform.GetChild(0).SetParent(Util.FindChildByName(Managers.Scenario.NPCs["환자"].gameObject, "basic_rig L Calf").transform, false);
-            point.transform.localPosition = new Vector3(0.0195f, 0.0195f, 0.0005f);
-            point.transform.localRotation = Quaternion.Euler(21.205f, 47.349f, 14.175f);
-            point.transform.localScale = new Vector3(0.0764116f, 0.01577159f, 0.07533843f);
-        }
-
-        if (parent == null)
-        {
-            Debug.LogWarning("Can't find transform");
-            return false;
-        }
+        
 
         Managers.Item.SelectedItem.Using = true;
 
@@ -115,70 +67,57 @@ public class Syringe : UsingItem
     {
         while (!check)
         {
-            if (go != null)
+            if (Input.GetMouseButtonDown(0))
             {
-                if (Input.GetMouseButtonDown(0))
+                // 바라보는 대상 확인
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, transform.forward, out hit))
                 {
-                    Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+                    // 바라보는 대상의 이름이 "환자"일 경우
+                    if (hit.transform.gameObject.name == "환자")
+                    {
+                        obj = hit.transform.gameObject;
+                    }
+                }
+                else
+                {
+                    if (obj != null)
+                    {
+                        obj = null;
+                    }
+                }
+                // 시나리오 액션이 "Use"일 때
+                if (Managers.Scenario.CurrentScenarioInfo.Action == "Use")
+                {
+                    // 올바른 오브젝트 클릭 체크
+                    if (obj != null && obj.transform.gameObject.name == "환자")
+                    {
+                        // 시나리오 정보가 유효할 때
+                        if (Managers.Scenario.CurrentScenarioInfo != null)
+                        {
+                            Mycharacter.State = CreatureState.Syringe;
 
-                    RaycastHit hitInfo;
-                    _layerPoint = LayerMask.GetMask("Point");
-                    //자식 오브젝트에 리지드바디 안달려있으면 부모껄로 인식됨
-                    if (Physics.Raycast(ray, out hitInfo, raycastDistance, _layerPoint))
-                    {
-                        obj = hitInfo.transform.gameObject;
-                        Debug.Log(obj);
-                    }
-                    else
-                    {
-                        if (obj != null)
-                        {
-                            obj = null;
+                            StartCoroutine(CoDelayIdle(3.0f));
                         }
                     }
-                    // 시나리오 액션이 "Use"일 때
-                    if (Managers.Scenario.CurrentScenarioInfo.Action == "Use")
+                    else if (!Managers.Item.IsInventoryOpen)
                     {
-                        // 올바른 오브젝트 클릭 체크
-                        if (obj != null && obj.transform.gameObject.name == "AccuratePoint")
-                        {
-                            // 시나리오 정보가 유효할 때
-                            if (Managers.Scenario.CurrentScenarioInfo != null)
-                            {
-                                Mycharacter.State = CreatureState.Syringe;
-                               
-                                StartCoroutine(CoDelayIdle(3.0f));
-                            }
-                        }
-                        else if (!Managers.Item.IsInventoryOpen)
-                        {
-                            Managers.UI.CreateSystemPopup("WarningPopup", "올바른 곳에 사용하세요.", UIManager.NoticeType.Warning);
-                        }
+                        Managers.UI.CreateSystemPopup("WarningPopup", "올바른 곳에 사용하세요.", UIManager.NoticeType.Warning);
                     }
                 }
             }
-            // 다음 프레임까지 대기
-            yield return null;
+           // 다음 프레임까지 대기
+           yield return null;
         }
     }
     IEnumerator CoDelayIdle(float time)
     {
-
         yield return new WaitForSeconds(time);
-        if (obj.name != "AccuratePoint")
-        {
-            Debug.Log("다른게 들어감");
-            yield break;
-        }
-
-        Managers.Scenario.Targets.Add("AccuratePoint");
-        
-        for (int i = 0; i < points.Count; i++)
-            points[i].SetActive(false);
 
         Managers.Item.ForceDropItem(ItemInfo);
         Managers.Scenario.MyAction = "Use";
         check = true;  // check를 true로 설정하여 완료
+        Managers.Scenario.Targets.Add("환자");
         if(Managers.Item.IsInventoryOpen == true)
         {
             Managers.Item.OpenOrCloseInventory();

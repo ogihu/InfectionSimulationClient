fe07111b381126afe5628647257f6a34818e55ed
Minuk-Kim-Dev <dduki0729@gmail.com>
2024-11-27 00:57:39 +0900

    Update Item.cs

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -2,7 +2,6 @@ using Google.Protobuf.Protocol;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Progress;
 
 public class Item : MonoBehaviour
 {
